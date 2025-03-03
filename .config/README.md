3a327d2b3207f1baab88eeea222cd279efa23b0b
BrrunoGuimasSz <brunoguimaraes.guigui@gmail.com>
2025-03-03 02:40:17 -0300

    more reorganization

--- .config/README.md
+++
@@ -1,6 +0,0 @@
-# Dotfiles
-
-Just my linux dotfiles, here are some screenshots:
-
-<img src="screenshot1.png">
-<img src="screenshot2.png">
--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require("config.lazy")
-require('mini.surround').setup()
-vim.cmd([[colorscheme monochrome]])
-vim.opt["tabstop"] = 4
-vim.opt["shiftwidth"] = 4
